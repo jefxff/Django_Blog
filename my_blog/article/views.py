a147f3e2f553c9bd0c82b11509c55cee43c829a5
jefxff <jefxff@outlook.com>
2019-06-10 14:15:17 +0800

    add Django blog project

---
+++ my_blog/article/views.py
@@ -0,0 +1,209 @@
+from django.shortcuts import render, redirect
+# 导入数据模型ArticlePost
+from .models import ArticlePost
+import markdown
+# 引入HttpResponse
+from django.http import HttpResponse
+# 引入刚才定义的ArticlePostForm表单类
+from .forms import ArticlePostForm
+# 引入User模型
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+# 引入分页模块
+from django.core.paginator import Paginator
+# 引入 Q 对象
+from django.db.models import Q
+from comment.models import Comment
+from .models import ArticleColumn
+
+
+def article_list(request):
+    # 从 url 中提取查询参数
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    column = request.GET.get('column')
+    tag = request.GET.get('tag')
+
+    # 初始化查询集
+    article_list = ArticlePost.objects.all()
+
+    # 搜索查询集
+    if search:
+        article_list = article_list.filter(
+            Q(title__icontains=search) |
+            Q(body__icontains=search)
+        )
+    else:
+        search = ''
+
+    # 栏目查询集
+    if column is not None and column.isdigit():
+        article_list = article_list.filter(column=column)
+
+    # 标签查询集
+    if tag and tag != 'None':
+        article_list = article_list.filter(tags__name__in=[tag])
+
+    # 查询集排序
+    if order == 'total_views':
+        article_list = article_list.order_by('-total_views')
+    #显示3篇文章（可以通过修改数值，来更改显示的文章数）
+    paginator = Paginator(article_list, 4)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    # 需要传递给模板（templates）的对象
+    context = {
+        'articles': articles,
+        'order': order,
+        'search': search,
+        'column': column,
+        'tag': tag,
+    }
+
+    return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+
+    # 取出文章评论
+    comments = Comment.objects.filter(article=id)
+    
+    # 浏览量 +1
+    article.total_views += 1
+    article.save(update_fields=['total_views'])
+
+    # 相邻发表文章的快捷导航
+    pre_article = ArticlePost.objects.filter(id__lt=article.id).order_by('-id')
+    next_article = ArticlePost.objects.filter(id__gt=article.id).order_by('id')
+    if pre_article.count() > 0:
+        pre_article = pre_article[0]
+    else:
+        pre_article = None
+
+    if next_article.count() > 0:
+        next_article = next_article[0]
+    else:
+        next_article = None
+
+    # 将markdown语法渲染成html样式
+    md = markdown.Markdown(
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        # 目录扩展
+        'markdown.extensions.toc',
+        ])
+    article.body = md.convert(article.body)
+
+    # 新增了md.toc对象
+    context = { 
+        'article': article,
+        'toc': md.toc,
+        'comments': comments,
+        'pre_article': pre_article,
+        'next_article': next_article,
+    }
+
+    return render(request, 'article/detail.html', context)
+
+
+# 检查登录
+@login_required(login_url='/userprofile/login/')
+# 写文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定目前登录的用户为作者
+            new_article.author = User.objects.get(id=request.user.id)
+            # 新增的代码
+            if request.POST['column'] != 'none':
+                new_article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 新增代码，保存 tags 的多对多关系
+            article_post_form.save_m2m()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        columns = ArticleColumn.objects.all()
+        # 赋值上下文
+        context = { 'article_post_form': article_post_form, 'columns': columns }
+        # 返回模板
+        return render(request, 'article/create.html', context)    
+
+# 删文章
+def article_delete(request, id):
+    # 根据 id 获取需要删除的文章
+    article = ArticlePost.objects.get(id=id)
+    # 调用.delete()方法删除文章
+    article.delete()
+    # 完成删除后返回文章列表
+    return redirect("article:article_list") 
+
+# 提醒用户登录
+@login_required(login_url='/userprofile/login/')
+def article_update(request, id):
+    """
+    更新文章的视图函数
+    通过POST方法提交表单，更新titile、body字段
+    GET方法进入初始表单页面
+    id： 文章的 id
+    """
+
+    # 获取需要修改的具体文章对象
+    article = ArticlePost.objects.get(id=id)
+    # 过滤非作者的用户
+    if request.user != article.author:
+        return HttpResponse("抱歉，你无权修改这篇文章。")
+
+    # 判断用户是否为 POST 提交表单数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存新写入的 title、body 数据并保存
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            if request.POST['column'] != 'none':
+                # 保存文章栏目
+                article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            else:
+                article.column = None
+
+            article.save()
+            # 完成后返回到修改后的文章中。需传入文章的 id 值
+            return redirect("article:article_detail", id=id)
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+
+    # 如果用户 GET 请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文，将 article 文章对象也传递进去，以便提取旧的内容
+        columns = ArticleColumn.objects.all()
+        context = { 
+            'article': article, 
+            'article_post_form': article_post_form,
+            'columns': columns,
+        }
+        # 将响应返回到模板中
+        return render(request, 'article/update.html', context)             
